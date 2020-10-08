af3eaac719a5a1e2e4fb95488a79660dbe36144e
MatrosovDenis <51385717+MatrosovDenis@users.noreply.github.com>
2020-10-08 22:44:53 +0300

    interface-refactoring

--- views.py
+++ views.py
@@ -3,187 +3,92 @@ from werkzeug.utils import secure_filename
 from app import app
 import os
 
-user = {'username':'test_user'}
+
+class User:
+    def __init__(self, username='test_user', status=False, firstname="test", lastname="user", email="test_user@email.com"):
+        self.is_signed = status
+        self.username = username
+        self.firstname = firstname
+        self.lastname = lastname
+        self.email = email
+
+
+class Check_settings:
+    def __init__(self, s1=True, s2=True, s3=True, s4=True, s5=True, s6=True, s7=True):
+        self.s1 = s1
+        self.s2 = s2
+        self.s3 = s3
+        self.s4 = s4
+        self.s5 = s5
+        self.s6 = s6
+        self.s7 = s7
+
+
+main_list = []
+
+for i in range(32):
+    main_list.append({
+        'name': 'File' + str(i),
+        'id': i
+    })
+
+user = User()
+check_setting = Check_settings()
 
 
 @app.route('/results/<id>')
 def results(id):
-    results = [
-        {
+    results = []
+    if check_setting.s1:
+        results.append({
             'name': 'Check 1',
             'res': 'Ok'
-        },
-        {
+        })
+    if check_setting.s2:
+        results.append({
             'name': 'Check 2',
             'res': 'Ok'
-        },
-        {
+        })
+    if check_setting.s3:
+        results.append({
             'name': 'Check 3',
             'res': 'Ok'
-        },
-        {
+        })
+    if check_setting.s4:
+        results.append({
             'name': 'Check 4',
             'res': 'Ok'
-        },
-        {
+        })
+    if check_setting.s5:
+        results.append({
             'name': 'Check 5',
             'res': 'Ok'
-        },
-        {
+        })
+    if check_setting.s6:
+        results.append({
             'name': 'Check 6',
             'res': 'Ok'
-        },
-        {
+        })
+    if check_setting.s7:
+        results.append({
             'name': 'Check 7',
             'res': 'Ok'
-        }
-    ]
-    return render_template("checking.html", title="Checking results", results=results)
+        })
+    return render_template("checking.html", title="Checking results", results=results, id=id)
 
 
 @app.route('/list')
 @app.route('/list/<page>')
 def list(page):
-    main_list = [
-        {
-            'name': 'File 1',
-            'id': 1
-        },
-        {
-            'name': 'File 2',
-            'id': 2
-        },
-        {
-            'name': 'File 3',
-            'id': 3
-        },
-        {
-            'name': 'File 4',
-            'id': 4
-        },
-        {
-            'name': 'File 5',
-            'id': 5
-        },
-        {
-            'name': 'File 6',
-            'id': 6
-        },
-        {
-            'name': 'File 7',
-            'id': 7
-        },
-        {
-            'name': 'File 8',
-            'id': 8
-        },
-        {
-            'name': 'File 9',
-            'id': 9
-        },
-        {
-            'name': 'File 10',
-            'id': 10
-        },
-        {
-            'name': 'File 11',
-            'id': 11
-        },
-        {
-            'name': 'File 12',
-            'id': 12
-        },
-        {
-            'name': 'File 13',
-            'id': 13
-        },
-        {
-            'name': 'File 14',
-            'id': 14
-        },
-        {
-            'name': 'File 15',
-            'id': 15
-        },
-        {
-            'name': 'File 16',
-            'id': 16
-        },
-        {
-            'name': 'File 17',
-            'id': 17
-        },
-        {
-            'name': 'File 18',
-            'id': 18
-        },
-        {
-            'name': 'File 19',
-            'id': 19
-        },
-        {
-            'name': 'File 20',
-            'id': 20
-        },
-        {
-            'name': 'File 21',
-            'id': 21
-        },
-        {
-            'name': 'File 22',
-            'id': 22
-        },
-        {
-            'name': 'File 23',
-            'id': 23
-        },
-        {
-            'name': 'File 24',
-            'id': 24
-        },
-        {
-            'name': 'File 25',
-            'id': 25
-        },
-        {
-            'name': 'File 26',
-            'id': 26
-        },
-        {
-            'name': 'File 27',
-            'id': 27
-        },
-        {
-            'name': 'File 28',
-            'id': 28
-        },
-        {
-            'name': 'File 29',
-            'id': 29
-        },
-        {
-            'name': 'File 30',
-            'id': 30
-        },
-        {
-            'name': 'File 31',
-            'id': 31
-        },
-        {
-            'name': 'File 32',
-            'id': 32
-        }
-    ]
-
     list = []
 
-    if int(page)*10 < len(main_list):
+    if int(page) * 10 < len(main_list):
         for number in range(10):
-            list.append(main_list[(int(page)-1)*10+number])
-        next_page=True
+            list.append(main_list[(int(page) - 1) * 10 + number])
+        next_page = True
     else:
-        for number in range(len(main_list) - ((int(page)-1)*10)):
-            list.append(main_list[(int(page)-1)*10+number])
+        for number in range(len(main_list) - ((int(page) - 1) * 10)):
+            list.append(main_list[(int(page) - 1) * 10 + number])
         next_page = False
 
     if int(page) == 1:
@@ -191,7 +96,8 @@ def list(page):
     else:
         prev_page = True
 
-    return render_template("list.html", title="Data base", list=list, page_number = int(page), next_page=next_page, prev_page=prev_page)
+    return render_template("list.html", title="Data base", list=list, page_number=int(page), next_page=next_page,
+                           prev_page=prev_page, user=user)
 
 
 @app.route('/home')
@@ -201,13 +107,18 @@ def home():
 
 @app.route('/')
 def main_page():
-    return render_template("main_page.html")
+    if user.is_signed:
+        return render_template("main_page.html", user=user)
+    else:
+        return render_template("main_page.html")
 
 
 @app.route('/sign-in', methods=["GET", "POST"])
 def sign_in():
     if request.method == "POST":
         print(request.form)
+        user.is_signed = True
+        user.username = "test_user"
         return redirect("/home")
     else:
         return render_template("sign-in.html")
@@ -217,6 +128,11 @@ def sign_in():
 def sign_up():
     if request.method == "POST":
         print(request.form)
+        user.is_signed = True
+        user.username = request.form['username']
+        user.email = request.form['email']
+        user.firstname = request.form['firstName']
+        user.lastname = request.form['lastName']
         return redirect("/home")
     else:
         return render_template("sign-up.html")
@@ -228,7 +144,7 @@ def presentation():
     data = file.stream.read()
     print(os.path.splitext(file.filename)[-1])
     if os.path.splitext(file.filename)[-1] not in [".ppt", ".pptx", ".odp", ".odpx"]:
-        return render_template("upload_status.html", title="Upload status", success=False, user=user), 400
+        return render_template("upload_presentation.html", title="Upload presentation", success=False, user=user), 400
     with open(secure_filename(file.filename), "wb") as output_file:
         output_file.write(data)
     return render_template("upload_status.html", title="Upload status", success=True, user=user)
@@ -236,5 +152,49 @@ def presentation():
 
 @app.route('/upload_presentation')
 def upload_presentation():
-    return render_template("upload_presentation.html", title="Upload presentation", user=user)
+    return render_template("upload_presentation.html", title="Upload presentation", success=True, user=user)
 
+
+@app.route('/check_settings')
+def check_settings():
+    return render_template("check_settings.html", title="Check settings", user=user)
+
+
+@app.route('/profile_settings')
+def profile_settings():
+    return render_template("profile_settings.html", title="Profile settings", user=user)
+
+
+@app.route('/log_out', methods=["GET", "POST"])
+def log_out():
+    user.is_signed = False
+    return redirect("/")
+
+
+@app.route('/check_set', methods=["GET", "POST"])
+def check_set():
+    check_setting.s1 = request.form.get('Check1')
+    check_setting.s2 = request.form.get('Check2')
+    check_setting.s3 = request.form.get('Check3')
+    check_setting.s4 = request.form.get('Check4')
+    check_setting.s5 = request.form.get('Check5')
+    check_setting.s6 = request.form.get('Check6')
+    check_setting.s7 = request.form.get('Check7')
+    return redirect("/home")
+
+
+@app.route('/profile_set', methods=["GET", "POST"])
+def profile_set():
+    user.username = request.form['username']
+    user.email = request.form['email']
+    user.firstname = request.form['firstName']
+    user.lastname = request.form['lastName']
+    return redirect("/home")
+
+
+@app.route('/delete_one/<id>')
+def delete_one(id):
+    for i in main_list:
+        if int(i.get('id')) == int(id):
+            main_list.remove(i)
+    return redirect("/list/1")
